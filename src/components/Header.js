aaa587d9e4b8f35b97aa178bed5f59604e2d6e50
HeshanChamika <it22194930@my.sliit.lk>
2024-11-29 18:37:39 +0530

    Update header logo and modify page title and favicon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Logo from '../assets/logo.svg'
+import Logo from '../assets/logo2.png'
 
 const Header = () => {
   return <header className='py-8'>
     <div className='container mx-auto'>
       <div className='flex justify-between items-center'>
-        <a href='#'>
-          <img src={Logo} alt='' />
+        <a href='home'>
+          <img src={Logo} className='h-10' alt='' />
         </a>
 
         <button className='btn btn-sm'>Work with me</button>
